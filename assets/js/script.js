e01ca43cc4a79c733ac9af52187d19655ac330f4
Michael Walker <michaelwwalker42@gmail.com>
2021-12-02 17:15:17 -0700

    add function to color-code each time block to indicate whether it is in the past, present, or future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,21 @@
 // show current date 
-$("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+$("#currentDay").text(moment().format('dddd,  MMMM Do YYYY'));
+
+var currentHour = moment().hour();
+
+function setTaskColor() {
+    // loop over time blocks and add class attributes for past, present, and future
+    $(".time-block").each(function () {
+        var taskHour = parseInt($(this).attr("id"));
+
+        if (taskHour > currentHour) {
+            $(this).addCalss("future");
+        } else if (taskHour === currentHour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+    })
+};
+
+setTaskColor();
